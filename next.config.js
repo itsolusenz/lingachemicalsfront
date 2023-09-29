291c26843a293ae633f5e4cd600a79c8276fdb6b
itsolusenz <itsolusenz1@gmail.com>
2023-09-29 18:19:28 +0530

    bug fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-const isProduction = process.env.NODE_ENV === 'production';
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
 
-module.exports = {
-    basePath: '/react/porto/demo3',
-    trailingSlash: isProduction
-} 
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+
+module.exports = nextConfig
